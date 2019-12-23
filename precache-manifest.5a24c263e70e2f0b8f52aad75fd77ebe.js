1f6143591e5ef984c234071ea837e94256bfb715
DaNuwa <yhan7788@gmail.com>
2019-12-23 23:11:05 +0530

    Updates

---
+++ precache-manifest.5a24c263e70e2f0b8f52aad75fd77ebe.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3d99bb5b3109529a4be4d2a44d16f55c",
+    "url": "/Finalbrain/index.html"
+  },
+  {
+    "revision": "4ac25d9dcc77142ddc48",
+    "url": "/Finalbrain/static/css/2.eb45ad44.chunk.css"
+  },
+  {
+    "revision": "a1a1e5b545c28453df96",
+    "url": "/Finalbrain/static/css/main.a9a2d419.chunk.css"
+  },
+  {
+    "revision": "4ac25d9dcc77142ddc48",
+    "url": "/Finalbrain/static/js/2.5edd8db9.chunk.js"
+  },
+  {
+    "revision": "a1a1e5b545c28453df96",
+    "url": "/Finalbrain/static/js/main.e742b1cf.chunk.js"
+  },
+  {
+    "revision": "e83ffc494794794e95f6",
+    "url": "/Finalbrain/static/js/runtime-main.8946a417.js"
+  },
+  {
+    "revision": "7f28e34423d4829ebef05b05ebbaad9e",
+    "url": "/Finalbrain/static/media/brain.7f28e344.jpg"
+  }
+]);
